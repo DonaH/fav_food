cb0fabb8b0725f24a078b5cfdb68fd82411e3bea
dona <dona.hsieh@gmail.com>
2016-05-09 22:59:30 -0700

    updated with env variables

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -3,8 +3,36 @@ class UploadsController < ApplicationController
   end
 
   def create
+    @picture = Picture.new(picture_params)
+
+    if @friend.save
+      redirect_to @picture, notice: 'Picture was successfully created.'
+    else
+      render action: 'new'
   end
 
+  private
+
   def index
+  end
+end
+
+class FriendsController < ApplicationController
+  # Other CRUD actions omitted
+
+  def create
+    @friend = Friend.new(friend_params)
+
+    if @friend.save
+      redirect_to @friend, notice: 'Friend was successfully created.'
+     else
+       render action: 'new'
+    end
+  end
+
+  private
+
+  def friend_params
+    params.require(:friend).permit(:avatar, :name)
   end
 end
