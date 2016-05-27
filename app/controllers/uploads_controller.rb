b13fb22fe83a0e9fcc2dbbd5911f1f8d6ce66e8b
dona <dona.hsieh@gmail.com>
2016-05-27 12:03:24 -0700

    added dotenv gem and modified aws-sdk version to prep for S3 upload of images

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -5,7 +5,7 @@ class UploadsController < ApplicationController
   def create
     @picture = Picture.new(picture_params)
 
-    if @friend.save
+    if @picture.save
       redirect_to @picture, notice: 'Picture was successfully created.'
     else
       render action: 'new'
@@ -15,24 +15,7 @@ class UploadsController < ApplicationController
 
   def index
   end
-end
 
-class FriendsController < ApplicationController
-  # Other CRUD actions omitted
-
-  def create
-    @friend = Friend.new(friend_params)
-
-    if @friend.save
-      redirect_to @friend, notice: 'Friend was successfully created.'
-     else
-       render action: 'new'
-    end
-  end
-
-  private
-
-  def friend_params
-    params.require(:friend).permit(:avatar, :name)
-  end
+  def picture_params
+    params.require(:picture).permit(:picture)
 end
